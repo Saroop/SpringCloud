f95fc5f79d733406b2413846a03e75c2440d4513
Saroop <saroopmtr@gmail.com>
2020-04-23 19:03:07 +0530

    Fallback method implemented using hystrix command Added actuator dependency to stream hystrix metrics

--- SampleApp-1/src/main/java/com/example/demo/resource/SampleAppResource.java
+++ SampleApp-1/src/main/java/com/example/demo/resource/SampleAppResource.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/sample-app-1")
 public class SampleAppResource {
@@ -17,11 +19,16 @@ public class SampleAppResource {
 	
 	@GetMapping()
 	@RequestMapping("/{name}")
+	@HystrixCommand(fallbackMethod="fallbackForGetMessage")
 	public String getMessage(@PathVariable final String name) {
 		
 		ResponseEntity<String> response = restTemplate.getForEntity("http://sample-app2/sample-app-2/" + name, String.class);
 		
 		return response.getBody();
+	}
+	
+	public String fallbackForGetMessage(final String name) {
+		return "SampleApp2 service is down, try after sometime !!!";
 	}
 
 }
